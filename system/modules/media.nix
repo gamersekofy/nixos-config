75552fd1c08408a628d66dbdb4b736438260b87e
Uzair Mohammed <uzair.hamed@gmail.com>
2024-12-17 19:51:00 -0800

    1: Updated media.nix to enable the OBS virtual camera

--- system/modules/media.nix
+++ system/modules/media.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{config, ...}: {
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -19,8 +15,14 @@
     #media-session.enable = true;
   };
 
+  # To enable the OBS Virtual Camera
   boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
   boot.kernelModules = [
     "v4l2loopback"
   ];
+
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 }
