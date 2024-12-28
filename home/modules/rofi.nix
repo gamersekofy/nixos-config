745a8468c3e94d3592e4928168d130e0356b6357
Uzair Mohammed <uzair.hamed@gmail.com>
2024-12-28 13:42:39 -0800

    1: Replaced rofi with rofi-wayland

--- home/modules/rofi.nix
+++ home/modules/rofi.nix
@@ -1,13 +1,5 @@
 {pkgs, ...}: {
-  programs.rofi = {
-    enable = true;
-    cycle = false;
-    plugins = with pkgs; [
-      rofi-emoji-wayland
-      rofi-calc
-      rofi-bluetooth
-      rofi-screenshot
-      rofi-power-menu
+    home.packages = [
+        pkgs.rofi-wayland	
     ];
-  };
 }
