4dd887b26f79c55048884d13f70f9b034c797bdb
Uzair Mohammed <uzair.hamed@gmail.com>
2025-01-13 08:31:18 -0800

    1: Updated LM Studio desktop entry properly

--- home/desktop/lmstudio.nix
+++ home/desktop/lmstudio.nix
@@ -1,7 +1,12 @@
 {
   # Override LM Studio entry to always launch with Wayland
-  xdg.desktopEntries.lmstudio = {
+  xdg.desktopEntries.lm-studio = {
     name = "LM Studio";
     exec = "lmstudio --enable-features=UseOzonePlatform --ozone-platform=wayland %U";
+    terminal = false;
+    type = "Application";
+    icon = "lm-studio";
+    categories = [ "Utility" ];
+    comment = "Discover, download, and run LLMs locally";
   };
 }
