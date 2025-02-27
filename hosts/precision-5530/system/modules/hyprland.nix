b5b383c0c5cb86199b34c74ca8944b82a123f82c
Uzair Mohammed <uzair.hamed@gmail.com>
2025-02-26 19:20:53 -0800

    1: Added Walker cache

--- hosts/precision-5530/system/modules/hyprland.nix
+++ hosts/precision-5530/system/modules/hyprland.nix
@@ -9,6 +9,17 @@
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 
+  # For Walker
+  nix.settings = {
+    substituters = ["https://walker.cachix.org"];
+    trusted-public-keys = ["walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="];
+  };
+
+  nix.settings = {
+    substituters = ["https://walker-git.cachix.org"];
+    trusted-public-keys = ["walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="];
+  };
+
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
