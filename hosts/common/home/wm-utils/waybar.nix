1931a562774bb2a9bce3e15cb5b3b18485661115
Uzair Mohammed <uzair.hamed@gmail.com>
2025-03-08 17:48:32 -0800

    Major overhaul to default Waybar style

--- hosts/common/home/wm-utils/waybar.nix
+++ hosts/common/home/wm-utils/waybar.nix
@@ -26,7 +26,7 @@
         "clock" = {
           timezone = "America/Los_Angeles";
           tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-          format = " {:%H:%M} 󰃭 {:%A %b %e}";
+          format = " {:%H:%M} 󰃭 {:%a, :%b :%e}";
         };
 
         # ----------------------------------------------
@@ -90,163 +90,175 @@
     };
 
     style = ''
-            @define-color base   #1e1e2e;
-            @define-color mantle #181825;
-            @define-color crust  #11111b;
+    @define-color base #1e1e2e;
+    @define-color mantle #181825;
+    @define-color crust #11111b;
 
-            @define-color text     #cdd6f4;
-            @define-color subtext0 #a6adc8;
-            @define-color subtext1 #bac2de;
+    @define-color text #cdd6f4;
+    @define-color subtext0 #a6adc8;
+    @define-color subtext1 #bac2de;
 
-            @define-color surface0 #313244;
-            @define-color surface1 #45475a;
-            @define-color surface2 #585b70;
+    @define-color surface0 #313244;
+    @define-color surface1 #45475a;
+    @define-color surface2 #585b70;
 
-            @define-color overlay0 #6c7086;
-            @define-color overlay1 #7f849c;
-            @define-color overlay2 #9399b2;
+    @define-color overlay0 #6c7086;
+    @define-color overlay1 #7f849c;
+    @define-color overlay2 #9399b2;
 
-            @define-color blue      #89b4fa;
-            @define-color lavender  #b4befe;
-            @define-color sapphire  #74c7ec;
-            @define-color sky       #89dceb;
-            @define-color teal      #94e2d5;
-            @define-color green     #a6e3a1;
-            @define-color yellow    #f9e2af;
-            @define-color peach     #fab387;
-            @define-color maroon    #eba0ac;
-            @define-color red       #f38ba8;
-            @define-color mauve     #cba6f7;
-            @define-color pink      #f5c2e7;
-            @define-color flamingo  #f2cdcd;
-            @define-color rosewater #f5e0dc;
+    @define-color blue #89b4fa;
+    @define-color lavender #b4befe;
+    @define-color sapphire #74c7ec;
+    @define-color sky #89dceb;
+    @define-color teal #94e2d5;
+    @define-color green #a6e3a1;
+    @define-color yellow #f9e2af;
+    @define-color peach #fab387;
+    @define-color maroon #eba0ac;
+    @define-color red #f38ba8;
+    @define-color mauve #cba6f7;
+    @define-color pink #f5c2e7;
+    @define-color flamingo #f2cdcd;
+    @define-color rosewater #f5e0dc;
 
-      * {
-         font-family: JetBrainsMono NF;
-      	 font-size: 15px;
-      }
+    * {
+    	font-family: JetBrainsMono NF;
+    	font-size: 15px;
+    }
 
-      #custom-power {
-         background-color: @base;
-      	 color: @red;
-      	 padding: 0.5rem 1rem;
-      	 margin: 5px 0;
-      	 border-radius: 5px 20px 20px 5px;
-      }
+    #battery {
+    	color: @base;
+    	background-color: @teal;
+    	margin: 5px;
+    	padding: 5px;
+    }
 
-      #battery {
-         color: @green;
-      }
+    #battery.charging {
+    	background-color: @green;
+    	color: @base;
+    }
 
-      #battery.charging {
-      	 color: @green;
-      }
+    #battery.warning:not(.charging),
+    #battery.critical:not(.charging) {
+    	background-color: @red;
+    	color: @base;
+    }
 
-      #battery.warning:not(.charging), #battery.critical:not(.charging) {
-         background-color: @red;
-      	 color: @base;
-      }
+    #waybar {
+    	background: @base;
+    	color: @text;
+    	margin: 1rem;
+    }
 
-      #waybar {
-        background: transparent;
-        color: @text;
-        margin: 5px 5px;
-      }
+    #workspaces {
+    	margin: 5px;
+    	background-color: @surface0;
+    	margin-left: 0.5rem;
+    	border-radius: 0rem;
+    }
 
-      #workspaces {
-        border-radius: 1rem;
-        margin: 5px;
-        background-color: @surface0;
-        margin-left: 1rem;
-      }
+    #workspaces button {
+    	color: @text;
+    	border-radius: 0rem;
+    }
 
-      #workspaces button {
-        color: @text;
-        border-radius: 1rem;
-        padding: 0.4rem;
-      }
+    #workspaces button.active {
+    	color: @surface0;
+    	background-color: @mauve;
+    }
 
-      #workspaces button.active {
-        color: @mauve;
-        border-radius: 1rem;
-      }
+    #workspaces button:hover {
+    	color: @mauve;
+    	border-radius: 0rem;
+    }
 
-      #workspaces button:hover {
-        color: @sapphire;
-        border-radius: 1rem;
-      }
+    #tray {
+    	background-color: @surface0;
+    	border-radius: 0rem;
+    	margin: 5px;
+    }
 
-      #custom-music,
-      #tray,
-      #backlight,
-      #clock,
-      #battery,
-      #pulseaudio,
-      #custom-lock,
-      #clock {
-        color: @blue;
-        border-radius: 0px 1rem 1rem 0px;
-        margin-right: 1rem;
-      }
+    #clock {
+    	color: @base;
+    	background-color: flamingo;
+    	border-radius: 0rem;
+    	margin: 5px;
+    	padding: 5px;
+    }
 
-      #backlight {
-        color: @yellow;
-      }
+    #backlight {
+    	background-color: @peach;
+    	margin: 5px;
+    	padding: 5px;
+    	color: @base;
+    }
 
-      #backlight, #battery {
-          border-radius: 0;
-      }
+    #bluetooth.off {
+    	color: @red;
+    }
 
-      #bluetooth.off {
-        color: @red;
-      }
+    #bluetooth.on {
+    	color: @blue;
+    }
 
-      #bluetooth.on {
-        color: @blue;
-      }
+    #bluetooth.connected {
+    	color: @blue;
+    }
 
-      #bluetooth.connected {
-        color: @blue;
-      }
+    #pulseaudio {
+    	background-color: @lavender;
+    	color: @base;
+    	border-radius: 0px;
+    	margin: 5px;
+    	padding: 5px;
+    }
 
-      #pulseaudio {
-        color: @maroon;
-        border-radius: 1rem 0px 0px 1rem;
-        margin-left: 1rem;
-      }
+    #pulseaudio.muted {
+    	background-color: @maroon;
+    	color: @base;
+    }
 
-      #custom-music {
-        color: @mauve;
-        border-radius: 1rem;
-      }
+    #custom-music {
+    	color: @mauve;
+    	border-radius: 1rem;
+    }
 
-      #custom-lock {
-          border-radius: 1rem 0px 0px 1rem;
-          color: @lavender;
-      }
+    #custom-lock {
+    	border-radius: 1rem 0px 0px 1rem;
+    	color: @lavender;
+    }
 
-      #custom-power {
-          margin-right: 1rem;
-          border-radius: 0px 1rem 1rem 0px;
-          color: @red;
-      }
+    #custom-power {
+    	margin: 5px;
+    	margin-right: 6px;
+    	color: @base;
+    	border-radius: 0rem;
+    	background-color: @red;
+    }
 
-      #tray {
-        margin-right: 1rem;
-        border-radius: 1rem;
-      }
+    #network {
+    	color: @text;
+    	background-color: @surface1;
+    	margin: 5px;
+    	padding: 5px;
+    }
 
-      #network {
-         color: @text;
-      }
+    #network.wifi,
+    #network.ethernet,
+    #network.linked {
+    	background-color: @blue;
+    	color: @base;
+    }
 
-      #network.disabled {
-          color: @red;
-      }
+    #network.disabled {
+    	color: @base;
+    	background-color: @red;
+    }
 
-      #network.disconnected {
-          color: @red;
-      }
+    #network.disconnected {
+    	color: @base;
+    	background-color: @red;
+    }
     '';
   };
 }
