456913b25f9749c768810d64518233d4fc397d56
Uzair Mohammed <uzair.hamed@gmail.com>
2025-03-19 19:00:16 -0700

    Removed glaxnimate

--- hosts/precision-5530/home/modules/misc.nix
+++ hosts/precision-5530/home/modules/misc.nix
@@ -2,20 +2,14 @@
   home.packages = with pkgs; [
     keepassxc
     yubioath-flutter
-
     pciutils
     glxinfo
     clinfo
     wayland-utils
     aha
-    fwupd
-    glaxnimate
     ddcutil
-
     galaxy-buds-client
-
     fastfetch
-
     lmstudio
   ];
 }
