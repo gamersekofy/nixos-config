af80f6194ea5e190d942fcea437fd86dec30bf4c
Uzair Mohammed <uzair.hamed@gmail.com>
2025-07-05 21:25:23 -0700

    Added Helix editor with two Catppuccin themes

---
+++ hosts/common/home/editors/helix.nix
@@ -0,0 +1,63 @@
+{pkgs, lib, ...}: {
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+
+    # Tell file picker to ignore these files or directories
+    ignores = [
+      # Rust's target directory
+      "target/"
+    ];
+
+    languages = {
+      # Enable Clippy on save
+      language-server.rust-analyzer.config = {
+        check = {command = "clippy";};
+        cargo = {features = "all";};
+      };
+
+      language-server.nixd = with pkgs; {
+        command = "${nixd}/bin/nixd";
+      };
+
+      language = [
+        {
+          name = "nix";
+          formatter.command = "alejandra";
+          language-servers = ["nixd"];
+        }
+      ];
+    };
+
+    themes = {
+      catppuccin_mocha = builtins.readFile ./themes/catppuccin_mocha.toml;
+    };
+
+    settings = {
+      theme = "catppuccin_mocha";
+
+      editor = {
+        end-of-line-diagnostics = "hint";
+
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        indent-guides = {
+          render = true;
+          character = "┊";
+        };
+
+        inline-diagnostics = {
+          cursor-line = "warning";
+          other-lines = "error";
+        };
+
+        # Tell file picker to show hidden files and directories
+        file-picker.hidden = false;
+      };
+    };
+  };
+}
