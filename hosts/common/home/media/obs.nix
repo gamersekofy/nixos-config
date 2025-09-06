4ae63b0acd9793060fb692bd999abed991333edb
Uzair Mohammed <uzair.hamed@gmail.com>
2025-09-05 19:41:20 -0700

    Moved general OBS declaration to common directory

---
+++ hosts/common/home/media/obs.nix
@@ -0,0 +1,11 @@
+{pkgs}: {
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs; [
+      obs-studio-plugins.obs-pipewire-audio-capture
+      obs-studio-plugins.obs-backgroundremoval
+      obs-studio-plugins.input-overlay
+      obs-studio-plugins.obs-vaapi
+    ];
+  };
+}
--- hosts/precision-5530/home/modules/media.nix
+++ hosts/precision-5530/home/modules/media.nix
@@ -14,13 +14,6 @@
   ];
 
   programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs; [
-      obs-studio-plugins.obs-pipewire-audio-capture
-      obs-studio-plugins.obs-backgroundremoval
-      obs-studio-plugins.input-overlay
-      obs-studio-plugins.obs-vaapi
-    ];
     package = pkgs.obs-studio.override {
       cudaSupport = true;
     };
