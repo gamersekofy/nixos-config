d6e384d27d9f2a0d8da44692793a0f193fd5807b
Uzair Mohammed <uzair.hamed@gmail.com>
2026-04-12 22:07:10 -0700

    Added configuration for Ghostty

---
+++ hosts/common/home/dev/ghostty.nix
@@ -0,0 +1,14 @@
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "light:Catppuccin Latte,dark:Catppuccin Mocha";
+
+      font-family = "CaskaydiaCove Nerd Font";
+      font-size = 11;
+      
+      window-height = 35;
+      window-width = 111;
+    };
+  };
+}
