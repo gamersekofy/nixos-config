e106104d4e02d1d55cfe8b15df9449a6562facd9
Uzair Mohammed <uzair.hamed@gmail.com>
2026-04-14 16:25:20 -0700

    Precision 5560: Enabled thermald

--- hosts/common/system/powerManagement.nix
+++ hosts/common/system/powerManagement.nix
@@ -1,11 +1,11 @@
 {lib, ...}: {
   powerManagement = {
     enable = true;
-    powertop.enable = true;
   };
 
   services = {
     power-profiles-daemon.enable = true;
     tlp.enable = lib.mkForce false;
+    thermald.enable = true;
   };
 }
