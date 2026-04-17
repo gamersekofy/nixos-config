d35e2c13fb0d7eddf51701b02a7764f3f409b257
Uzair Mohammed <uzair.hamed@gmail.com>
2026-04-17 15:42:09 -0700

    Precision 5560: NOCTALIA- added Set as Noctalia wallpaper option to Yazi

--- hosts/precision-5560/home/modules/niri/noctalia/wallpaper-picker.nix
+++ hosts/precision-5560/home/modules/niri/noctalia/wallpaper-picker.nix
@@ -2,12 +2,30 @@
   pkgs,
   inputs,
   ...
-}: {
-  programs.yazi.settings.opener.set-wallpaper = [
-    {
-      run = "${inputs.noctalia.packages.pkgs.stdenv.hostPlatform.system.default}/bin/noctalia-shell ipc call wallpaper set \"$1\" all";
-      for = "linux";
-      desc = "Set as wallpaper";
-    }
-  ];
+}: let
+  system = pkgs.stdenv.hostPlatform.system;
+  noctalia-pkg = inputs.noctalia.packages.${system}.default;
+in {
+  programs.yazi = {
+    settings = {
+      opener = {
+        set-wallpaper = [
+          {
+            run = "${noctalia-pkg}/bin/noctalia-shell ipc call wallpaper set \"$1\" all";
+            for = "linux";
+            desc = "Set as Noctalia wallpaper";
+          }
+        ];
+      };
+
+      open = {
+        prepend_rules = [
+          {
+            mime = "image/*";
+            use = ["set-wallpaper" "open"];
+          }
+        ];
+      };
+    };
+  };
 }
