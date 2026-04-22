f8f141f9a69bbb4780d518251de990a9bdea18bd
Uzair Mohammed <uzair.hamed@gmail.com>
2026-04-22 09:25:32 -0700

    Added and configured Starship to use modified Jetpack preset

--- hosts/common/home/dev/starship.nix
+++ hosts/common/home/dev/starship.nix
@@ -1,7 +1,9 @@
-{
+{lib, ...}: {
   # Enable the starship prompt and use the default config
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    presets = [];
+    settings = import ./starship-presets/jetpack.nix {inherit lib;};
   };
 }
