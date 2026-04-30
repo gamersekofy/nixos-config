036be423ca8340f0def1615ce5d6a1582ddc38db
Uzair Mohammed <uzair.hamed@gmail.com>
2026-04-29 22:32:55 -0700

    Removed Evolution

--- hosts/common/system/noctalia.nix
+++ hosts/common/system/noctalia.nix
@@ -1,5 +1,4 @@
 {
-  imports = [./evolution.nix];
   nix.settings = {
     extra-substituters = ["https://noctalia.cachix.org"];
     extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
