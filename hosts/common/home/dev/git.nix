5decf032cc501a86cb576f2b0f72a912bda1f82a
Uzair Mohammed <uzair.hamed@gmail.com>
2026-09-11 17:45:34 -0700

    Modified Git settings to use updated key names

--- hosts/common/home/dev/git.nix
+++ hosts/common/home/dev/git.nix
@@ -1,5 +1,19 @@
 {
-  home.file.".ssh/id_ed25519_sk_signing.pub".text = ''
+  imports = [
+    ./ssh.nix
+  ];
+
+  programs.ssh = {
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519_sk_rk_github-auth_gamersekofy";
+      };
+    };
+  };
+
+  home.file.".ssh/id_ed25519_sk_rk_git-signing_gamersekofy.pub".text = ''
     sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICV8xaw0idrjzIb/SjSWTdND6IHbyfL0uMS1lb0BdBXCAAAAD3NzaDpnaXQtc2lnbmluZw== YubiKey Git Signing Key
   '';
 
@@ -21,7 +35,7 @@
 
     signing = {
       format = "ssh";
-      key = "~/.ssh/id_ed25519_sk_signing.pub";
+      key = "~/.ssh/id_ed25519_sk_rk_git-signing_gamersekofy.pub";
     };
   };
 
